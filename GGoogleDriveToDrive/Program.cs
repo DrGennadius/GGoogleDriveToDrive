965a623e3d977940abe1a0784f5aafe1192997e0
Gennady Zykov <zykovgennady@gmail.com>
2022-03-14 23:21:42 +0300

    Fix DateTime compare.

--- GGoogleDriveToDrive/Program.cs
+++ GGoogleDriveToDrive/Program.cs
@@ -222,7 +222,7 @@ namespace GGoogleDriveToDrive
                         GoogleFileInfo googleFileInfo = DbContext.GoogleFiles.Query().SingleOrDefault(x => x.GoogleId == gFile.Id);
                         if (googleFileInfo != null
                             && googleFileInfo.ModifiedTime.HasValue
-                            && googleFileInfo.ModifiedTime.Value.ToShortDateString() == gFile.ModifiedTime.Value.Date.ToShortDateString())
+                            && googleFileInfo.ModifiedTime.Value.ToString() == gFile.ModifiedTime.Value.Date.ToString())
                         {
                             return;
                         }
